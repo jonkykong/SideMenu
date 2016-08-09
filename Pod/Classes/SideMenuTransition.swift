3fc2e45ac73fdc476f91588bbd263b32dadb13e4
Maximilian Alexander <max@zinkpulse.com>
2016-08-08 17:47:31 -0700

    ensuring that the handling of gestures happen synchronously and not at different states

--- Pod/Classes/SideMenuTransition.swift
+++ Pod/Classes/SideMenuTransition.swift
@@ -320,7 +320,6 @@ internal class SideMenuTransition: UIPercentDrivenInteractiveTransition, UIViewC
             let tapView = UIView()
             tapView.autoresizingMask = [.FlexibleHeight, .FlexibleWidth]
             let exitPanGesture = UIPanGestureRecognizer()
-            exitPanGesture.enabled = SideMenuManager.enableGestures
             exitPanGesture.addTarget(SideMenuTransition.self, action:#selector(SideMenuTransition.handleHideMenuPan(_:)))
             let exitTapGesture = UITapGestureRecognizer()
             exitTapGesture.addTarget(SideMenuTransition.self, action: #selector(SideMenuTransition.handleHideMenuTap(_:)))
