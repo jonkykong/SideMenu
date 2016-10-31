c6236fa8b8a454754378db2adc06cfa8854ba580
Soheil <soheilbm@me.com>
2016-10-31 10:56:33 +0800

    Adding support for Carthage

---
+++ SideMenu/SideMenu.h
@@ -0,0 +1,19 @@
+//
+//  SideMenu.h
+//  SideMenu
+//
+//  Created by Soheil on 31/10/16.
+//  Copyright © 2016 jonkykong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SideMenu.
+FOUNDATION_EXPORT double SideMenuVersionNumber;
+
+//! Project version string for SideMenu.
+FOUNDATION_EXPORT const unsigned char SideMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SideMenu/PublicHeader.h>
+
+
