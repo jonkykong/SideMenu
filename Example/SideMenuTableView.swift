ecebaca9b90c96a110b4986694749028d3490e4d
jonkykong <jpkent@gmail.com>
2017-04-26 12:21:58 -0700

    Logic correction.

--- Example/SideMenuTableView.swift
+++ Example/SideMenuTableView.swift
@@ -14,6 +14,8 @@ class SideMenuTableView: UITableViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        print("SideMenu Appearing!")
+        
         // this will be non-nil if a blur effect is applied
         guard tableView.backgroundView == nil else {
             return
@@ -24,8 +26,6 @@ class SideMenuTableView: UITableViewController {
         imageView.contentMode = .scaleAspectFit
         imageView.backgroundColor = UIColor.black.withAlphaComponent(0.2)
         tableView.backgroundView = imageView
-        
-        print("SideMenu Appearing!")
     }
     
     override func viewDidAppear(_ animated: Bool) {
