baae6186d7e06f4b4dad424d2b94cc839d20a357
Mykola Vaniurskyi <mv@leobit.co>
2019-08-06 16:10:20 +0300

    Added isHidden property for public usage

--- Pod/Classes/UISideMenuNavigationController.swift
+++ Pod/Classes/UISideMenuNavigationController.swift
@@ -341,6 +341,11 @@ extension UISideMenuNavigationController: MenuModel {
         get { return _leftSide.value }
         set { _leftSide.value = newValue }
     }
+  
+    /// Indicates if the menu is anywhere in the view hierarchy, even if covered by another view controller.
+    public override var isHidden: Bool {
+        return super.isHidden
+    }
 
     @IBInspectable open var menuWidth: CGFloat {
         get { return settings.menuWidth }
