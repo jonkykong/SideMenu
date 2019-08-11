9d9c22c7866fc40b09c9325d0a6c4a58ceedccda
jonkykong <jpkent@gmail.com>
2019-08-11 01:06:29 -0700

    Updated tests

--- ExampleTests/ExampleTests.swift
+++ ExampleTests/ExampleTests.swift
@@ -10,6 +10,17 @@ import XCTest
 
 class ExampleTests: XCTestCase {
 
+    private let styleTitles = ["Slide In", "Slide Out", "In + Out", "Dissolve"]
+    private let swipeHere = "Swipe Here"
+
+    private let app = XCUIApplication()
+    private var mainViewController: XCUIElement {
+        return app.navigationBars[swipeHere]
+    }
+    private var mainViewControllerNavigation: XCUIElement {
+        return mainViewController.otherElements[swipeHere]
+    }
+
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -26,9 +37,46 @@ class ExampleTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testTapLeft() {
+        let elementsQuery = app.scrollViews.otherElements
+        for title in styleTitles {
+            elementsQuery.buttons[title].tap()
+            mainViewController.buttons["Left Menu"].tap()
+            app.tables/*@START_MENU_TOKEN@*/.staticTexts["Push View Controller 1"]/*[[".cells.staticTexts[\"Push View Controller 1\"]",".staticTexts[\"Push View Controller 1\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+            app.navigationBars["You Can Still Swipe!"].buttons[swipeHere].tap()
+            validate()
+        }
     }
 
+    func testTapRight() {
+        let elementsQuery = app.scrollViews.otherElements
+        for title in styleTitles {
+            elementsQuery.buttons[title].tap()
+            mainViewController.buttons["Right Menu"].tap()
+            app.tables/*@START_MENU_TOKEN@*/.staticTexts["Present View Controller 1"]/*[[".cells.staticTexts[\"Present View Controller 1\"]",".staticTexts[\"Present View Controller 1\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+            app.buttons["Dismiss"].tap()
+            validate()
+        }
+    }
+
+    func testSwiping() {
+        mainViewControllerNavigation.swipeRight()
+        let element = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element
+        element.swipeLeft()
+        validate()
+
+        element.swipeLeft()
+        mainViewControllerNavigation.swipeLeft()
+        element.swipeRight()
+        validate()
+    }
+
+    private func validate() {
+        XCTAssertTrue(mainViewController.exists)
+    }
+
+    /* TODO - More tests:
+     - Rotation
+     - All menu settings
+     */
 }
