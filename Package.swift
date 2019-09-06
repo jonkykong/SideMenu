d25de76a9231e87aff0a2a2fd25de2677b5a04af
Rafael Francisco <rafaelfrancisco6@gmail.com>
2019-09-06 15:24:53 +0100

    Added SwiftPM support

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SideMenu",
+    products: [
+        .library(name: "SideMenu",  targets: ["SideMenu"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "SideMenu", path: "Pod/Classes")
+    ]
+)
+
