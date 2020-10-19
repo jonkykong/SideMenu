51f6b88d43049662e9d46254b02d33e5aee8b2c5
jonkykong <jpkent@gmail.com>
2020-10-19 13:11:13 -0700

    Added SwiftUI target

---
+++ SideMenu (SwiftUI)/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SideMenu (SwiftUI)
+//
+//  Created by Jon Kent on 10/19/20.
+//  Copyright © 2020 jonkykong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SideMenu (SwiftUI)/SideMenu__SwiftUI_App.swift
@@ -0,0 +1,18 @@
+//
+//  SideMenu__SwiftUI_App.swift
+//  SideMenu (SwiftUI)
+//
+//  Created by Jon Kent on 10/19/20.
+//  Copyright © 2020 jonkykong. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SideMenu__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
